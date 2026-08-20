5fb5ec2a0582303db54fd0902b849ea88b49927f
ambisinistra <9372109@gmail.com>
2026-08-20 13:48:11 +0400

    initial commit. data split to train/val/test by board_ids (0.7 / 0.15 / 0.15)

---
+++ src/training/train_baseline.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+
+
+def train_baseline():
+    # 1. Загружаем легкую предобученную модель segmentation
+    model = YOLO("yolo11s-seg.pt")  # или yolo8s-seg.pt
+
+    # 2. Обучаем baseline
+    results = model.train(
+        data="configs/data.yaml",
+        epochs=50,
+        imgsz=640,
+        batch=16,
+        project="runs/segment",
+        name="baseline",
+        save=True,
+        plots=True,
+        seed=42,
+    )
+
+
+if __name__ == "__main__":
+
+    train_baseline()
